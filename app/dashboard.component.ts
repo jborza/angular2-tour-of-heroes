fac8cb530c2f0fc43e45cbc2f797c0a7a0260fd6
jborza@gmail.com <jborza>
2016-08-14 12:40:32 +0200

    working on routing according to tutorial

---
+++ app/dashboard.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {HeroService} from './hero.service';
+
+import {Hero} from './hero';
+
+@Component({
+    selector: '<my-dashboard>',
+    templateUrl: 'app/dashboard.component.html',
+})
+export class DashboardComponent implements OnInit{
+    heroes: Hero[] = [];
+
+    constructor(private heroService:HeroService){
+
+    }
+
+    ngOnInit(){
+        this.heroService.getHeroes().then(
+            heroes => this.heroes = heroes
+        );
+    }
+
+}
